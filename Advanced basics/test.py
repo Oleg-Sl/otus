91d72d6ef893577e6f96471b443bea869324bd2e
Oleg <slepcov_oleg@mail.ru>
2020-07-02 20:42:20 +0700

    Устранены замечания от 21.06.2020

--- Advanced basics/test.py
+++ Advanced basics/test.py
@@ -5,7 +5,7 @@ import shutil
 import gzip
 from collections import namedtuple
 
-from log_analyzer import logger, read_conf, search_last_log_file, read_log, parsing_string_log, formation_report, write_to_file
+from log_analyzer import read_conf, search_last_log_file, read_log, formation_report, write_to_file
 
 
 class TestReadConf(unittest.TestCase):
@@ -26,16 +26,16 @@ class TestReadConf(unittest.TestCase):
         shutil.rmtree('config_test', ignore_errors=True)
 
     def testNoExistsDir(self):
-        self.failIf(read_conf('_', self.config))
+        self.assertEqual(read_conf('_', self.config), self.config)
 
     def testExistsDir(self):
         self.assert_(read_conf('config_test', self.config))
 
     def testWriteConfig(self):
-        read_conf('config_test', self.config)
-        self.assertEqual(self.config['REPORT_SIZE'], 123)
-        self.assertEqual(self.config['REPORT_DIR'], './reports_test')
-        self.assertEqual(self.config['LOG_DIR'], './log_test')
+        test_config = read_conf('config_test', self.config)
+        self.assertEqual(test_config['REPORT_SIZE'], 123)
+        self.assertEqual(test_config['REPORT_DIR'], './reports_test')
+        self.assertEqual(test_config['LOG_DIR'], './log_test')
 
 
 class TestSearchLastLogFile(unittest.TestCase):
@@ -50,13 +50,14 @@ class TestSearchLastLogFile(unittest.TestCase):
 
     def testSerchLastDateFile(self):
         Data_file = namedtuple('Data_file', 'file date')
-        self.assertEqual(search_last_log_file('test_log', 'nginx-access-ui.log'),
+        self.assertEqual(search_last_log_file('test_log', '^(nginx-access-ui.log-)(?P<date>\d{8})(\.gz)?$'),
                          (Data_file('nginx-access-ui.log-20150616', datetime.datetime.strptime('20150616', '%Y%m%d'))))
 
     def testNotDir(self):
         self.failIf(search_last_log_file('_', '_'))
 
     def testNotFile(self):
+        print(search_last_log_file('test_log', '^(nginx)(\d{8})(\.gz)?$'))
         self.failIf(search_last_log_file('test_log', '_'))
 
 
@@ -64,8 +65,8 @@ class TestReadLog(unittest.TestCase):
     def setUp(self):
         os.makedirs('_test_file')
         data = """1.168.65.96 -  - [29/Jun/2017:03:50:23 +0300] "GET /api/v2/internal/banner/24288647/info HTTP/1.1" 200 351 "-" "-" "-" "1498697423-2539198130-4708-9752780" "89f7f1be37d" 0.072
-        1.169.137.128 -  - [29/Jun/2017:03:50:23 +0300] "POST /api/v2/banner/21456892 HTTP/1.1" 200 70795 "-" "Slotovod" "-" "1498697423-2118016444-4708-9752779" "712e90144abee9" 0.158
-        1.168.65.96 -  - [29/Jun/2017:03:50:23 +0300] "-" 200 293 "-" "-" "-" "1498697423-2539198130-4708-9752783" "89f7f1be37d" -"""
+1.169.137.128 -  - [29/Jun/2017:03:50:23 +0300] "POST /api/v2/banner/21456892 HTTP/1.1" 200 70795 "-" "Slotovod" "-" "1498697423-2118016444-4708-9752779" "712e90144abee9" 0.158
+1.168.65.96 -  - [29/Jun/2017:03:50:23 +0300] "-" 200 293 "-" "-" "-" "1498697423-2539198130-4708-9752783" "89f7f1be37d" -"""
         f1 = open(os.path.join('_test_file', 'test.log'), 'w')
         f1.write(data)
         f1.close()
@@ -77,91 +78,45 @@ class TestReadLog(unittest.TestCase):
         shutil.rmtree('_test_file', ignore_errors=True)
 
     def testReadFile(self):
-        data_lst = read_log(os.path.join('_test_file', 'test.log'))
-        d = {}
-        for key, value in data_lst:
-            d[key] = value
-        self.assertEqual(d['/api/v2/internal/banner/24288647/info'], 0.072)
-        self.assertEqual(d['/api/v2/banner/21456892'], 0.158)
-        self.assertEqual(d['total'], 3)
-        self.assertEqual(d['error'], 1)
+        data_lst = read_log(os.path.join('_test_file', 'test.log'), "\"(GET|POST|HEAD|PUT) (?P<url>\S+) \S+\".+ (?P<time>\d+\.\d+)\s{0,2}$")
+        self.assertEqual(data_lst.__next__(), ('/api/v2/internal/banner/24288647/info', 0.072))
+        self.assertEqual(data_lst.__next__(), ('/api/v2/banner/21456892', 0.158))
+        self.assertEqual(data_lst.__next__(), None)
+
 
     def testReadGzip(self):
-        data_lst = read_log(os.path.join('_test_file', 'test.gz'))
-        d = {}
-        for key, value in data_lst:
-            d[key] = value
-        self.assertEqual(d['/api/v2/internal/banner/24288647/info'], 0.072)
-        self.assertEqual(d['/api/v2/banner/21456892'], 0.158)
-        self.assertEqual(d['total'], 3)
-        self.assertEqual(d['error'], 1)
-
-
-class TestReadLog(unittest.TestCase):
-    def setUp(self):
-        self.line_1 = '1.168.65.96 -  - [29/Jun/2017:03:50:23 +0300] "GET /api/v2/internal/banner/24288647/info HTTP/1.1" 200 351 "-" "-" "-" "1498697423-2539198130-4708-9752780" "89f7f1be37d" 0.072'
-        self.line_2 = '1.169.137.128 -  - [29/Jun/2017:03:50:23 +0300] "POST /api/v2/banner/21456892 HTTP/1.1" 200 70795 "-" "Slotovod" "-" "1498697423-2118016444-4708-9752779" "712e90144abee9" 0.158'
-        self.line_3 = '1.168.65.96 -  - [29/Jun/2017:03:50:23 +0300] "-" 200 293 "-" "-" "-" "1498697423-2539198130-4708-9752783" "89f7f1be37d" "-"'
-
-    def testLinePOSTUrl(self):
-        self.assertEqual(parsing_string_log(self.line_1.split('"')), ('/api/v2/internal/banner/24288647/info', 0.072))
-
-    def testLineGETUrl(self):
-        self.assertEqual(parsing_string_log(self.line_2.split('"')), ('/api/v2/banner/21456892', 0.158))
-
-    def testInvalidLine(self):
-        self.assertEqual(parsing_string_log(self.line_3.split('"')), None)
+        data_lst = read_log(os.path.join('_test_file', 'test.gz'), "\"(GET|POST|HEAD|PUT) (?P<url>\S+) \S+\".+ (?P<time>\d+\.\d+)\s{0,2}$")
+        self.assertEqual(data_lst.__next__(), ('/api/v2/internal/banner/24288647/info', 0.072))
+        self.assertEqual(data_lst.__next__(), ('/api/v2/banner/21456892', 0.158))
+        self.assertEqual(data_lst.__next__(), None)
 
 
 class TestFormationReport(unittest.TestCase):
     def setUp(self):
-        self.lst = [('url_1', 3), ('url_2', 1), ('url_1', 4), ('url_3', 7), ('url_2', 6), ('url_4', 4), ('total', 10), ('error', 4)]
+        self.lst = [('url_1', 2), ('url_2', 6), ('url_1', 4), ('url_3', 12), ('url_2', 6)]
 
     def testGetReport(self):
         report = formation_report(self.lst, 2, 50)
         self.assertEqual(len(report), 2)
+        print(report)
         self.assertEqual(report[0],
-                         {'url': 'url_1',
-                          'count': 2,
-                          'count_perc': 20.0,
-                          'time_sum': 7,
-                          'time_perc': 28.0,
-                          'time_avg': 3.5,
-                          'time_max': 4,
-                          'time_med': 4})
-        self.assertEqual(report[1],
                          {'url': 'url_2',
                           'count': 2,
-                          'count_perc': 20.0,
-                          'time_sum': 7,
-                          'time_perc': 28.0,
-                          'time_avg': 3.5,
+                          'count_perc': 40.0,
+                          'time_sum': 12,
+                          'time_perc': 40.0,
+                          'time_avg': 6.0,
                           'time_max': 6,
                           'time_med': 6})
-
-    def testGetReport(self):
-        self.assertEqual(formation_report(self.lst, 2, 30), None)
-
-
-class TestWriteToFile(unittest.TestCase):
-    def setUp(self):
-        self.report = [{'time': 2}, {'time': 5}, {'time': 6}]
-        self.path_report = './_test_report'
-        self.date = datetime.datetime.strptime('20190623', '%Y%m%d')
-
-    def tearDown(self):
-        if os.path.exists(self.path_report):
-            shutil.rmtree(self.path_report, ignore_errors=True)
-
-    def testCreateFile(self):
-        write_to_file(self.report, self.path_report, self.date)
-        self.assert_(os.path.exists(self.path_report))
-        self.assert_(os.path.exists(os.path.join(self.path_report, 'report-2019.6.23.html')))
-
-        f = open(os.path.join(self.path_report, 'report-2019.6.23.html'))
-        data = f.read()
-        self.assert_(str(self.report) in data)
-        f.close()
+        self.assertEqual(report[1],
+                         {'url': 'url_3',
+                          'count': 1,
+                          'count_perc': 20.0,
+                          'time_sum': 12,
+                          'time_perc': 40.0,
+                          'time_avg': 12.0,
+                          'time_max': 12,
+                          'time_med': 12})
 
 
 if __name__ == "__main__":
